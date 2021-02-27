f5be91606af3c6ad4846fb349fd2a6cd834aed12
MannyAk1703 <61808828+MannyAk1703@users.noreply.github.com>
2021-02-27 17:20:30 +0000

    Fixed code and added tag

--- js/script.js
+++ js/script.js
@@ -7,8 +7,9 @@ project 1 - A Random Quote Generator
 //quotes are placed in an array, and each quotes is an object, with the variables being; quote, source, citation and year
 const quotes = [
   {
-    quote: "Do or do not, there is no try",
+    quote: "Do or do not, there is no try.",
     source: "Master Yoda",
+    profession: "Jedi Master",
     citation: "Star Wars: Episode VI – Return of the Jedi",
     year: 1983,
   },
@@ -16,12 +17,14 @@ const quotes = [
     quote:
       "It's the questions we can't answer that teach us the most. They teach us how to think. If you give a man an answer, all he gains is a little fact. But give him a question and he'll look for his own answers.",
     source: "Patrick Rothfuss",
+    profession: "Author",
     citation: "Wise Man's Fear",
     year: 2011,
   },
   {
-    quote: "With great power comes great responsibility",
+    quote: "With great power comes great responsibility.",
     source: "Uncle Ben",
+    profession: "Unfortunate By-stander",
     citation: "Spiderman",
     year: 2002,
   },
@@ -29,24 +32,28 @@ const quotes = [
     quote:
       "The greatest glory in living lies not in never falling, but in rising every time we fall.",
     source: "Nelson Mandela",
+    profession: "Politician",
     citation: "",
     year: "",
   },
   {
     quote: "Only a life lived for others is a life worthwhile.",
     source: "Albert Einstein",
+    profession: "Sciencetist",
     citation: "",
     year: "",
   },
   {
     quote: "There is some good in this world, and it’s worth fighting for.",
     source: "J.R.R Tolkien",
+    profession: "Author",
     citation: "The Two Towers",
     year: 1954,
   },
   {
     quote: "Stay hungry, stay foolish.",
     source: "Steve Jobs",
+    profession: "Entrepreneur",
     citation: "",
     year: "",
   },
@@ -54,30 +61,35 @@ const quotes = [
     quote:
       "This above all: To thine own self be true, And it must follow, as the night the day, Thou canst not then be false to any man.",
     source: "William Shakespeare",
+    profession: "Author",
     citation: "Hamlet",
-    year: 1600,
+    year: "",
   },
   {
     quote: "Beware; for I am fearless, and therefore powerful.",
     source: "Mary Shelly",
+    profession: "Author",
     citation: "Frankenstein",
-    year: 1818,
+    year: "",
   },
   {
     quote: "The future belongs to those who prepare for it today.",
     source: "Malcolm X",
+    profession: "Activist",
     citation: "",
     year: "",
   },
   {
     quote: "The successful warrior is the average man, with laser-like focus.",
     source: "Bruce Lee",
+    profession: "Actor",
     citation: "",
     year: "",
   },
   {
-    quote: "Anything worth dying for is certainly worth living for ",
+    quote: "Anything worth dying for is certainly worth living for.",
     source: "Joseph Heller",
+    profession: "Author",
     citation: "Catch-22",
     year: 1961,
   },
@@ -94,13 +106,11 @@ console.log(getRandomQuote());
 //This function makes alteration to the index.html file.
 //The message variable is left as an empty string and each tag corresponding to the objects variable is added
 function printQuote() {
-  let message = "";
+  let message = ``;
   let result = getRandomQuote(quotes);
   message = `<p class='quote'> ${result.quote} </p>`;
-  message += `<p class='source'> ${result.source}`;
-  message += `<span class='citation'> ${result.citation} </span>`;
-  message += `<span class='year'> ${result.year} </span>`;
-
+  message += `<p class='source'> ${result.source}<span class='citation'> ${result.citation} </span> <span class='profession'> ${result.profession} </span><span class='year'> ${result.year} </span> </p> `;
+  console.log(message);
   //This code generates three random numbers and place in an array
   randomRGB = `rgb( ${Math.floor(Math.random() * 256)}, ${Math.floor(
     Math.random() * 256
